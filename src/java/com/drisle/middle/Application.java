bc25688cf4e42e19c46f7dba7947685f4b94a38c
prashanth.a <prashanth.a@flipkart.com>
2015-03-26 12:28:15 +0530

    minor-fix

--- src/java/com/drisle/middle/Application.java
+++ src/java/com/drisle/middle/Application.java
@@ -4,10 +4,12 @@ package com.drisle.middle; /**
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 public class Application extends WebMvcConfigurerAdapter{
